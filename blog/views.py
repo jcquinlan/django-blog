0a4d7d683a5e60c0ec26813b2d9f872a6826fae8
James Quinlan <quinlanjc@vcu.edu>
2016-04-04 23:12:40 -0400

    add blog detail view

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from .models import Blog
 def post_list(request):
     posts = Blog.objects.all().order_by('created_date')
     return render(request, 'post_list.html', { "posts": posts })
+
+def post_detail(request, pk):
+    post = Blog.objects.get( pk=pk )
+    return render(request, 'post_detail.html', { 'post': post })
