fee0014dda56773b5dc07e672bd4060a4a270dd2
James Quinlan <quinlanjc@vcu.edu>
2016-04-05 14:37:30 -0400

    adding new post form

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Blog
@@ -8,5 +8,8 @@ def post_list(request):
     return render(request, 'post_list.html', { "posts": posts })
 
 def post_detail(request, pk):
-    post = Blog.objects.get( pk=pk )
+    post = get_object_or_404(Blog, pk=pk )
     return render(request, 'post_detail.html', { 'post': post })
+
+def post_new(request):
+    return(request, 'post_new.html', {'dog': 'dog!'})
