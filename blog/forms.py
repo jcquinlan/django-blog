c3007d7932fc8bd29e2374197fbaee36382d3e6c
James Quinlan <quinlanjc@vcu.edu>
2016-04-05 22:49:41 -0400

    add new post form

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from .models import Blog
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
-        field = ('title', 'text')
+        fields = ('title', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.utils import timezone
 
 from .models import Blog
+from .forms import BlogForm
 
 def post_list(request):
     posts = Blog.objects.all().order_by('created_date')
@@ -12,4 +14,19 @@ def post_detail(request, pk):
     return render(request, 'post_detail.html', { 'post': post })
 
 def post_new(request):
-    return(request, 'post_new.html', {'dog': 'dog!'})
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+
+        if form.is_valid():
+            # this line adds the title and text to the post, since we specified in
+            # models.py to create the form with the title and text inputs
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = BlogForm()
+
+    return render(request, 'post_new.html', {'form': form })
