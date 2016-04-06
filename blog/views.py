ab30001706c2d74e6532d62d8df55e1416876054
James Quinlan <quinlanjc@vcu.edu>
2016-04-05 23:11:12 -0400

    finished django girls tutorial

--- blog/views.py
+++ blog/views.py
@@ -30,3 +30,17 @@ def post_new(request):
         form = BlogForm()
 
     return render(request, 'post_new.html', {'form': form })
+
+def post_edit(request, pk):
+    post = get_object_or_404(Blog, pk = pk)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = BlogForm(instance = post)
+    return render(request, 'post_edit.html', {'form': form })
